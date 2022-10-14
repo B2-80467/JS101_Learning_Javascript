cea4170bafc344e8bdf39aee644d380963dffa96
sandy shinde <samirshinde011@gmail.com>
2022-10-14 20:19:02 +0530

    my first

---
+++ sam.js
@@ -0,0 +1,68 @@
+//problem 1;
+
+let a;
+
+
+a= "\"Masai School\"";
+b="\"A Transformation in Education\""
+console.log(a+"\n"+b);
+
+//problem2;
+let name="samir";
+
+console.log(name);
+
+var name1="Dadasaheb";
+
+console.log(name1);
+
+let name2="swati";
+
+console.log(name2);
+
+console.log("--------------");
+
+console.log(name+"\n"+name1+"\n"+name2);
+
+//problem3
+
+var x="samir";
+
+let age=24;
+
+console.log( x ) ;
+
+console.log(typeof(x));
+
+console.log( age ) ;
+
+console.log( typeof(age) ) ;
+
+
+console.log("----------");
+
+console.log(x+"\n"+ typeof(x)+"\n"+ age+"\n"+ typeof(age));
+
+//problem 4
+
+
+
+console.log("🅡🅔🅢🅤🅛🅣 🅣🅐🅑🅛🅔");
+
+
+
+
+
+let x="Name=samir.";
+
+const schoole ="school=K.B.P.V.S.Nager.";
+
+const gread = "gread = 10.";
+
+const section="section = scince.";
+
+const rollNo="rollno=401.";
+
+let a="markes obtained in Phy,Bio,Chem.";
+
+var phy="Phy=40.", bio="Bio=50.", chem="Chem=60." ;
